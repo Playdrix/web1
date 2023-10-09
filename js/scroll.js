f2ed8460f12e7ff2181214c57e07367cf6acb7a7
aplicationqv <142430234+aplicationqv@users.noreply.github.com>
2023-10-09 16:58:18 -0500

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,29 @@
+const header = document.querySelector("header");
+const mainContent = document.querySelector("main");
+
+let lastScrollY = window.scrollY;
+let setScroll = 0;
+
+function handleScroll() {
+    const scrollY = window.scrollY || window.pageYOffset;
+
+    console.log("scrollY: " + scrollY)
+    console.log("lastScrollY: " + lastScrollY)
+    console.log("setScroll: " + setScroll)
+    console.log("header: " + header.style.transform)
+
+    if (scrollY > lastScrollY) {
+        // El usuario está bajando el scroll, ocultar el encabezado
+        header.style.transform = `translateY(-${window.scrollY}px)`;
+    } else {
+        // El usuario está subiendo el scroll, mostrar el encabezado
+        header.style.transform = `translateY(${setScroll}px)`;
+        lastScrollY = setScroll
+    }
+
+    lastScrollY = scrollY;
+}
+
+window.addEventListener("scroll", handleScroll);
+
+handleScroll();
